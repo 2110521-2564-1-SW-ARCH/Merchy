d33aad7c4c7b6a084156fdf81afe2779521d60ae
PatipanB <patipan.b7@gmail.com>
2021-09-28 07:49:50 +0000

    Fix update page

--- Gateway/gateway/views.py
+++ Gateway/gateway/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import User
 from .forms import RegistationForm
@@ -10,15 +10,15 @@ def landing_page(request):
 
 
 def all_users(request):
-    users = User.objects.all(
-    
+    users = User.objects.all()
     return render(request, 'gateway/users.html', {
             'users': users,
-        })
+    })
+
 
 def user_details(request, user_id):
     # selected_user = User.objects.get(id=user_id)
-    selected_user = get_object_or_404(User,pk=user_id)
+    selected_user = get_object_or_404(User, pk=user_id)
     return render(request, 'gateway/user-details.html', {
         'fname': selected_user.fname,
         'lname': selected_user.lname,
@@ -28,29 +28,44 @@ def user_details(request, user_id):
         'role': selected_user.get_role_display(),
     })
 
+
 def add_user(request):
-    registration_form = RegistationForm(request.POST)
-    if registration_form.is_valid():
-         registration_form.save()
+    registration_form = RegistationForm()
+    if request.method == 'POST':
+        registration_form = RegistationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            users = User.objects.all()
+            return render(request, 'gateway/users.html', {
+                'users': users,
+            })
     return render(request, 'gateway/add-user.html', {
         'form': registration_form,
     })
 
+
 def update_user(request, user_id):
-     # fetch the object related to passed id
-    selected_user = get_object_or_404(User,id=user_id)
+    # fetch the object related to passed id
+    selected_user = get_object_or_404(User, id=user_id)
     # pass the object as instance in form
-    registration_form = RegistationForm(request.POST or None, instance = selected_user)
-    
-    if registration_form.is_valid():
-        registration_form.save()
-        return HttpResponseRedirect("/gateway/users/"+user_id)
-    
-    return render(request, '/gateway/update-user.html', {
-        'form': registration_form,
-    })
+
+    registration_form = RegistationForm(request.POST or None,
+                                        instance=selected_user)
+
+    if request.method == 'POST':
+        if registration_form.is_valid():
+            print('Form Valid')
+            registration_form.save()
+            return all_users(request)
+
+    context = {
+        'form': registration_form
+    }
+
+    return render(request, 'gateway/update-user.html', context)
+
 
 def del_user(request, user_id):
-    del_user = User.objects.filter(id=user_id).delete()
-    return  all_users(request)
+    User.objects.filter(id=user_id).delete()
+    return all_users(request)
 
