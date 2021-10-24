c212c30f50192c1fc8be5afc74134bfa992f1962
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-10-24 23:58:23 +0700

    Remove tutorials app

--- gateway/tutorials/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    fname = models.CharField(max_length=255, blank=False, default='')
-    lname = models.CharField(max_length=255, blank=False, default='')
-    email = models.EmailField()
-
