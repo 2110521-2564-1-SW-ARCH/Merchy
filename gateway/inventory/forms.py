78d6d3a2e9514d40ca000cd66226b5763a9deb72
patipanb <patipan.b7@gmail.com>
2021-10-24 16:28:14 +0700

    Add gate/inventory service

---
+++ gateway/inventory/forms.py
@@ -0,0 +1,9 @@
+from django.core.validators import MinValueValidator
+from django import forms
+
+
+class EntryForm(forms.Form):
+    name = forms.CharField(label='Product', max_length=100)
+    amount = forms.IntegerField(label='Amount')
+    price = forms.FloatField(label='Price', validators=[MinValueValidator(0.0)])
+    note = forms.CharField(label="Note", max_length=200)
