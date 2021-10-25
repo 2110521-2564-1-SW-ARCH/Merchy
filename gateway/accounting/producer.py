ff00110ebb602fcc06e0377448a71d78d5aa1b8a
SuchonC <suchon_foke@windowslive.com>
2021-10-26 01:02:09 +0700

    gateway/accounting adapter
    Co-authored-by: Patipan Buranangura <PatipanB@users.noreply.github.com>
    Co-authored-by: Pabhanuwat Pongsawad <pabhanuwat@gmail.com>

---
+++ gateway/accounting/producer.py
@@ -0,0 +1,10 @@
+import json
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')
+channel = connection.channel()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='likes', body=json.dumps(body), properties=properties)
