781916e4a08695bc422b03bfe8d784bad3762b9f
SuchonC <suchon_foke@windowslive.com>
2021-10-25 08:58:46 +0700

    add Token model

--- ordertracking/models.py
+++ ordertracking/models.py
@@ -1,5 +1,12 @@
 from datetime import datetime
+from enum import Enum
 from pydantic import BaseModel
+import db
+
+class TokenName(str, Enum):
+    THPOST_REQUEST_TOKEN = "thpost_dynamic_token"
+    THPOST_WEBHOOK_TOKEN = "thpost_webhook_token"
+
 
 class Order(BaseModel):
     arg1: str
@@ -7,3 +14,11 @@ class Order(BaseModel):
 
     def __str__ (self):
         return f"Haha {self.arg1} {self.arg2}"
+
+class Token(BaseModel):
+    expire: datetime
+    token: str
+    name: TokenName
+
+    def insert_or_update(self):
+        return db.insert_or_update_token(self)
