8087d90d4f5476542a094f256458540a07e78d79
SuchonC <suchon_foke@windowslive.com>
2021-10-25 08:57:21 +0700

    reformat

--- ordertracking/thpost.py
+++ ordertracking/thpost.py
@@ -1,23 +1,27 @@
 import requests
 import json
 import os
+import db
+from models import Token, TokenName
 
 STATIC_TOKEN = os.getenv("THPOST_STATIC_TOKEN")
 test_barcodes = ["EY145587896TH", "RC338848854TH"]
 
+
 def track_by_barcodes(barcodes):
-    my_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IkF1dGhvcml6YXRpb24iLCJleHAiOjE2Mzc3NTM4MjEsInJvbCI6WyJST0xFX1VTRVIiXSwiZCpzaWciOnsicCI6InpXNzB4IiwicyI6bnVsbCwidSI6ImI0ZWViZjJiZTYxYTQ3NTMyNDkzOGFmZjQxZWQ2NTllIiwiZiI6InhzeiM5In19.TN7O77JP_U8R2teWN6Jery372zglxP0InnlLU63srRUeR50UHYIjTs-68PVpzZCwtVF35hhbNz_kwzkUROqXKQ"
+    request_token = "test"
     headers = {
-        "Authorization": f"Token {my_token}",
-        "Content-type": "application/json"
+        "Authorization": f"Token {request_token}",
+        "Content-type": "application/json",
     }
-    body = json.dumps({
-        "status": "all",
-        "language": "TH",
-        "barcode": barcodes
-    })
-    r = requests.post("https://trackapi.thailandpost.co.th/post/api/v1/track", headers=headers, data=body).json()
-    if r['status'] == False: return Null
+    body = json.dumps({"status": "all", "language": "TH", "barcode": barcodes})
+    r = requests.post(
+        "https://trackapi.thailandpost.co.th/post/api/v1/track",
+        headers=headers,
+        data=body,
+    ).json()
+    if r["status"] == False:
+        return None
     return r
 
 
@@ -30,27 +34,44 @@ def get_token(static_token):
     """
     headers = {
         "Authorization": f"Token {static_token}",
-        "Content-type": "application/json"
+        "Content-type": "application/json",
     }
-    return requests.post("https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token", headers=headers).json()
+    token = requests.post(
+        "https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token",
+        headers=headers,
+    ).json()
+    return Token(**token, name=TokenName.THPOST_REQUEST_TOKEN).insert_or_update()
+
 
 def get_webhook_token(static_token):
     headers = {
         "Authorization": f"Token {static_token}",
-        "Content-type": "application/json"
+        "Content-type": "application/json",
     }
-    return requests.post("https://trackwebhook.thailandpost.co.th/post/api/v1/authenticate/token", headers=headers).json()
+    token = requests.post(
+        "https://trackwebhook.thailandpost.co.th/post/api/v1/authenticate/token",
+        headers=headers,
+    ).json()
+    return Token(**token, name=TokenName.THPOST_WEBHOOK_TOKEN).insert_or_update()
+
 
 def subscribe_barcodes(barcodes):
-    webhook_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IkF1dGhvcml6YXRpb24iLCJleHAiOjE2Mzc3NjI0NjAsInJvbCI6WyJST0xFX1VTRVIiXSwiZCpzaWciOnsicCI6InpXNzB4IiwicyI6bnVsbCwidSI6ImI0ZWViZjJiZTYxYTQ3NTMyNDkzOGFmZjQxZWQ2NTllIiwiZiI6InhzeiM5In19.FY_NJJWI7BqHdxtVUM7PZSgnhIyKf7TLR8Jh352n3uNu8R1xuWWsEsxbllogLIS8mbtXXASJWtjjMDejcd_HnA"
+    webhook_token = "test"
     headers = {
         "Authorization": f"Token {webhook_token}",
-        "Content-type": "application/json"
+        "Content-type": "application/json",
     }
-    body = json.dumps({
-        "status": "all",
-        "language": "TH",
-        "barcode": barcodes,
-        "req_previous_status": False
-    })
-    return requests.post("https://trackwebhook.thailandpost.co.th/post/api/v1/hook", headers=headers, body=body)
+    body = json.dumps(
+        {
+            "status": "all",
+            "language": "TH",
+            "barcode": barcodes,
+            "req_previous_status": False,
+        }
+    )
+    return requests.post(
+        "https://trackwebhook.thailandpost.co.th/post/api/v1/hook",
+        headers=headers,
+        body=body,
+    )
+
