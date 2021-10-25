b62f99c23f8192d26c3d0ed4fc3c5d6254939965
SuchonC <suchon_foke@windowslive.com>
2021-10-25 11:35:02 +0700

    refactor main app

--- ordertracking/main.py
+++ ordertracking/main.py
@@ -1,33 +1,32 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-
 from typing import List
 from fastapi import FastAPI, Query
-from enum import Enum
+from models.enum import Courier
 import os
 import uvicorn
+from controllers import thpost
+from utils.db import close_conn
 
-import thpost
+app = FastAPI(on_shutdown=[close_conn])
+PORT = int(os.getenv("PORT"))
 
+# first use
+# user -> register -> retrieve orders from selling platforms -> get order status -> insert orders to db along with status
 
-app = FastAPI()
-
-PORT = os.getenv("PORT")
-
-
-class Courier(str, Enum):
-    thpost = "thpost"
-
+# later
+# webhook -> update(append) orders status
 
 @app.get("/")
 def read_root():
+    print(thpost.get_token(thpost.STATIC_TOKEN))
     return "order tracking works"
 
 
 @app.get("/trackbybarcodes/{courier}")
 def thpost_track_by_barcodes(courier: Courier, barcodes: List[str] = Query(...)):
-    if courier == Courier.thpost:
+    if courier == Courier.THPOST:
         return thpost.track_by_barcodes(barcodes)
 
 
