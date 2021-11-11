1df2880add91b28dffbc6b1cec21542d00b857a0
SuchonC <suchon_foke@windowslive.com>
2021-11-11 15:19:39 +0700

    reformat

--- inventory/controllers/item.js
+++ inventory/controllers/item.js
@@ -1,23 +1,23 @@
 const Item = require('../models/Item');
 const grpc = require('@grpc/grpc-js');
 
-module.exports.getAllItems = async function({ request }, cb) {
+module.exports.getAllItems = async function ({ request }, cb) {
     const items = await Item.find();
     cb(null, { items });
 };
 
-module.exports.getItem = async function({ request: { id } }, cb) {
+module.exports.getItem = async function ({ request: { id } }, cb) {
     const item = await Item.findById(id);
     if (item) cb(null, item);
     else cb({ code: grpc.status.NOT_FOUND, details: 'Not found' });
 };
 
-module.exports.createItem = async function({ request }, cb) {
+module.exports.createItem = async function ({ request }, cb) {
     const createdItem = await Item.create(request);
     cb(null, createdItem);
 };
 
-module.exports.updateItem = async function({ request: { id, ...others } }, cb) {
+module.exports.updateItem = async function ({ request: { id, ...others } }, cb) {
     const existingItem = await Item.findById(id);
     if (existingItem) {
         const updatedItem = await Item.findByIdAndUpdate(id, others, { new: true });
@@ -27,7 +27,7 @@ module.exports.updateItem = async function({ request: { id, ...others } }, cb) {
     }
 };
 
-module.exports.deleteItem = async function({ request: { id } }, cb) {
+module.exports.deleteItem = async function ({ request: { id } }, cb) {
     const deletedEntryItem = await Item.findByIdAndDelete(id);
     if (deletedEntryItem) cb(null, {});
     else cb({ code: grpc.status.NOT_FOUND, details: 'NOT Found' });
