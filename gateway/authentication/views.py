d80f9182cbc00d9298d8141d95f137093636d1fd
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-11-18 00:27:38 +0700

    <feat>: separate Authentication service adapter from authentication handler

--- gateway/authentication/views.py
+++ gateway/authentication/views.py
@@ -2,16 +2,10 @@ from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
  
-from authentication.models import User
-from authentication.serializers import UserSerializer
-from utils.http_local import create_requests_with_header, AuthService
 from utils.auth import verify_token
 from utils.decorators import jwt_verified
 from rest_framework.decorators import api_view
-
-import requests,os
-
-# MUST implement authorization header for every path
+from authentication.adapter import AuthService
 
 AuthService = AuthService()
 
@@ -39,35 +33,12 @@ def user_list(request):
         data = AuthService.delete_user(id)
         return JsonResponse(data)
  
- 
-@api_view(['GET', 'PUT', 'DELETE'])
-@jwt_verified(['GET', 'PUT', 'DELETE'])
-def user_detail(request, pk):
-    r = create_requests_with_header(request)
- 
-    # GET / PUT / DELETE
-    if request.method == 'GET':
-        user = r.get(f'http://localhost:3001/api/user/{pk}').json()
-        return JsonResponse(user)
-
-    elif request.method == 'PUT':
-        token = request.COOKIES.get('token')
-        user_data = JSONParser().parse(request) 
-        updated_user = r.put(f'http://localhost:3001/api/user/{pk}', data=user_data).json()
-        return JsonResponse(updated_user)
-
-    elif request.method == 'DELETE': 
-        data = r.delete(f'http://localhost:3001/api/user/{pk}').json()
-        return JsonResponse(data)
-
 @api_view(['POST'])
 def login(request):
-    r = create_requests_with_header(request)
     if request.method == 'POST':
         credential = JSONParser().parse(request)
         data = AuthService.login(credential)
-        # data = r.post('http://localhost:3001/api/login',data=credential)
-        if data.text == "Unauthorized": return JsonResponse({"message": data.text})
+        if data.text == "Unauthorized": return JsonResponse({"message": data.text}, status=401)
         else: 
             response = JsonResponse(data.json())
             token = data.json()['token']
