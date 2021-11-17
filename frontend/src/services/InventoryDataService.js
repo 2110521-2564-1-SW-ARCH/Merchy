deab7928b168ee0ca1af768de531f9e38ead9e2e
Pabhanuwat Pongsawad <pabhanuwat@gmail.com>
2021-11-18 00:37:40 +0700

    <feat>: create inventory service adapter

---
+++ frontend/src/services/InventoryDataService.js
@@ -0,0 +1,35 @@
+import http from "../http-common";
+
+class InventoryDataService {
+
+    async getAllEntries() {
+        return await http.get("/entry")
+    }
+
+    async getOneEntry(entryId) {
+        return await http.get(`/entry/${entryId}`)
+    }
+
+    async getAllItems() {
+        return await http.get("/item")
+    }
+
+    async getOneItem(itemId) {
+        return await http.get(`/item/${itemId}`)
+    }
+
+    async createItem(data) {
+        return await http.post(`/item`,data)
+    }
+    
+    async updateItem(itemId,data) {
+        return await http.put(`/item/${itemId}`, data)
+    }
+
+    async deleteItem(itemId) {
+        return await http.delete(`/item/${itemId}`)
+    }
+
+}
+
+export default new InventoryDataService();
