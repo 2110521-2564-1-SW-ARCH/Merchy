a7cf3f67e72e33acd7411b7c623822e235d2f13e
SuchonC <suchon_foke@windowslive.com>
2021-11-18 22:54:21 +0700

    change import name

--- gateway/ordertracking/views.py
+++ gateway/ordertracking/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework.decorators import api_view
-from utils.http_local import create_requests_with_header
+from utils.http_common import create_requests_with_header
 
 # body
 # {
