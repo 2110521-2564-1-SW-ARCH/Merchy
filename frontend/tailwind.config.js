f41bf2a7ebaf92d72a94b0f2dfc321c58b2ba940
authorites <authorites@outlook.co.th>
2021-11-20 23:50:20 +0700

    [Mod] enable :active bg-change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      padding: ['hover'],
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
