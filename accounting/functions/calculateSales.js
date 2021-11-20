f8f758a9e3615f6b4022305ace69c582376f67d5
SuchonC <suchon_foke@windowslive.com>
2021-11-20 16:54:21 +0700

    accounting now able to calculate sales

--- accounting/functions/calculateSales.js
+++ accounting/functions/calculateSales.js
@@ -7,6 +7,7 @@ ORDER_TRACKING_PORT = process.env.ORDER_TRACKING_PORT
 ORDER_TRACKING_PROTOL = process.env.ORDER_TRACKING_PROTOL
 ORDER_TRACKING_URL = `${ORDER_TRACKING_PROTOL}://${ORDER_TRACKING_IP}:${ORDER_TRACKING_PORT}`
 
+// startDate and endDate must be a str of ISO8601 format
 async function getAllOrders(startDate = null, endDate = null) {
     let { data } = await axios.get(`${ORDER_TRACKING_URL}/orders`, {
         params: {
@@ -18,7 +19,40 @@ async function getAllOrders(startDate = null, endDate = null) {
     return data
 }
 
+function getScaleNotation(createdAt, scale) {
+    createdAt = dayjs(createdAt)
+    switch (scale) {
+        case "year":
+            return createdAt.format("YYYY")
+        case "month":
+            return createdAt.format("YYYY-MM")
+        case "day":
+            return createdAt.format("YYYY-MM-DD")
+    }
+}
+
+function sortObject(obj) {
+    return Object.keys(obj).sort().reduce(function (result, key) {
+        result[key] = obj[key]
+        return result
+    }, {})
+}
+
 module.exports.calculateSales = async function (request) {
-    let orders = await getAllOrders(request.startDate, request.endDate)
-    return orders
+    if (!request.scale) return { success: false, message: "Missing scale parameter" }
+    let response = {}
+    let { scale } = request
+    let { orders } = await getAllOrders(request.startDate, request.endDate)
+
+    for (order of orders) {
+        let orderPrice = Number(order.price)
+        let orderCreatedAt = order.createdAt
+        let scaleNotation = getScaleNotation(orderCreatedAt, scale)
+        // if response.year exists then add orderPrice
+        if (response[scaleNotation]) response[scaleNotation] += orderPrice
+        // else create response.year with orderPrice
+        else response[scaleNotation] = orderPrice
+    }
+
+    return sortObject(response)
 }
