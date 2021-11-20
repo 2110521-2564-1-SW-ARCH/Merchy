ee0e9ba8830b4c96098dc27e26f0215cbfedf280
SuchonC <suchon_foke@windowslive.com>
2021-11-20 17:21:06 +0700

    refactor gateway/acounting in amqp part

--- gateway/accounting/amqp.py
+++ gateway/accounting/amqp.py
@@ -1,40 +1,23 @@
 import pika
 import json
+import os
 
-HOST = "localhost"
+HOST = os.getenv("RABBIT_MQ_IP")
+PRODUCING_QUEUE = "request"
+CONSUMING_QUEUE = "response"
 
-
-def produce(queue, msg):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue, exclusive=False)
-    channel.basic_publish(exchange="", routing_key="request", body=json.dumps(msg))
-    connection.close()
-
-
-class Consumer:
-    def __init__(self, *args, **kwargs):
-        self.conn = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-        self.channel = self.conn.channel()
-
-    def consume(self, queue, callback):
-        self.channel.queue_declare(queue)
-        self.channel.basic_consume(
-            queue=queue, on_message_callback=callback, auto_ack=True
-        )
-        self.channel.start_consuming()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        print('haha')
-        self.channel.close()
-        self.conn.close()
+producing_connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+producing_channel = producing_connection.channel()
+producing_channel.queue_declare(queue=PRODUCING_QUEUE, exclusive=False)
 
 
-def consume(queue, cb):
+def produce(msg):
+    producing_channel.basic_publish(exchange="", routing_key=PRODUCING_QUEUE, body=json.dumps(msg))
 
-    with Consumer() as consumer:
-        consumer.consume(queue=queue, callback=cb)
-        print("here")
+
+def consume(cb):
+    consuming_connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST))
+    consuming_channel = consuming_connection.channel()
+    consuming_channel.queue_declare(queue=CONSUMING_QUEUE)
+    consuming_channel.basic_consume(queue=CONSUMING_QUEUE, on_message_callback=cb, auto_ack=True)
+    consuming_channel.start_consuming()
