1a80f1154d5a4795c7853b2a6abdfb12fa31d1dc
patipanb <patipan.b7@gmail.com>
2021-11-23 22:50:09 +0700

    fix ordertable container

--- frontend/src/views/Order.vue
+++ frontend/src/views/Order.vue
@@ -1,10 +1,9 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-
   <div class=" flex flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+    <div class="my-10 overflow-x-auto sm:mx-12 lg:mx-16">
+      <div class="align-middle inline-block min-w-full">
+        <div class="shadow overflow-hidden border-2 border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
@@ -23,23 +22,20 @@
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Buyer name
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+               <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Address
-                </th>
+                </th> -->
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Payment Method
                 </th>
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Price
                 </th>
-                <th scope="col" class="relative px-6 py-3">
-                  <span class="sr-only">Actions</span>
-                </th>
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
                 <tr v-for="order in orders">
-                  <td class="whitespace-nowrap">
+                  <td class="px-6 py-4 whitespace-nowrap">
                       {{ order.platform }}
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
@@ -54,9 +50,9 @@
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     {{ order.addressShipping.firstName }}
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                 <!-- <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 truncate">
                     {{ order.addressShipping.country }} - {{order.addressShipping.city}}
-                  </td>
+                  </td> -->
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                     {{ order.paymentMethod }}
                   </td>
@@ -81,6 +77,7 @@
 <script>
 import InventoryDataService from '../services/InventoryDataService'
 import * as dayjs from "dayjs"
+import axios from "axios"
 
 export default {
     name: 'Inventory',
@@ -93,6 +90,7 @@ export default {
     methods: {
         getAllOrders: async function() {
             const response = await InventoryDataService.getAllEntries()
+            console.log(await axios.get("http://localhost:3000/api/accounting?resourceType=sales&startDate=2020-11-19T00:00:00&endDate=2021-11-19T23:59:59&scale=year", {withCredentials: true}))
             this.orders = response.data.orders
         }
     },
